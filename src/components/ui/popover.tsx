25c78d776f2ad1099b500ea08a4514ac824469d5
cloudmwana <cloudmwana@gmail.com>
2025-05-26 14:48:19 +0200

    first commit

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -11,7 +11,7 @@ const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content> & { variant?: 'default' | 'glass' | 'intense' }
 >(({ className, align = "center", sideOffset = 4, variant = 'default', ...props }, ref) => {
-  const baseClass = "z-50 w-72 rounded-md border p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2";
+  const baseClass = "z-[9999] w-72 rounded-md border p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2";
 
   const variantClass =
     variant === 'glass' ? 'glass-card' :
@@ -25,9 +25,8 @@ const PopoverContent = React.forwardRef<
         align={align}
         sideOffset={sideOffset}
         className={cn(baseClass, variantClass, className)}
-        avoidCollisions={false}
-        collisionPadding={20}
-        sticky="always"
+        avoidCollisions={true}
+        collisionPadding={10}
         {...props}
       />
     </PopoverPrimitive.Portal>
